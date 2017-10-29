2599cc62d4f7cadb93dd6011067dca01e1ca21b6
khanh.dao <khanh.dao@move.com>
2017-10-29 10:38:18 -0700

    Merge heads

---
+++ client/modules/Static/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+// import { connect } from 'react-redux';
+
+class HomePage extends React.Component {
+
+  render() {
+    return (
+      <h1>Hi this is khanh</h1>
+    );
+  }
+
+}
+
+// This is written as stateless React
+export default HomePage;
